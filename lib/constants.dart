991d48d8bec9d515168763fd906dd0f718c196ad
elziorocha <rochasenzo.04@gmail.com>
2024-10-30 02:39:57 -0300

    first commit

---
+++ lib/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+Color apprimaryColor = const Color(0xFF9D0DA2);
+
+Color appsecondaryColor = const Color(0xFFFAC2FC);
+Color textColor = const Color(0xFF343434);
---
+++ lib/telas/Home/homepage.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_final/telas/Home/Widget/home_appbar.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: 35,
+              ),
+              CustomAppBar()
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
